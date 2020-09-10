0bac83876461129db6ef980d899c704347038b51
Jon Bates <spadger@gmail.com>
2020-09-11 00:50:42 +0100

    Adding some json support

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,32 +1,33 @@
 import com.typesafe.config.ConfigFactory
 import io.github.config4k.extract
-import io.ktor.application.call
-import io.ktor.html.respondHtml
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.http.HttpStatusCode
+import io.ktor.response.*
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import kotlinx.html.*
-
-fun HTML.index() {
-    head {
-        title("Hello from Ktor!")
-    }
-    body {
-        div {
-            +"Hello from Ktor"
-        }
-    }
-}
+import java.time.Instant
 
 fun main() {
     val config = ConfigFactory.load().extract<OverallConfig>()
 
     embeddedServer(Netty, port = config.port) {
+
+        install(ContentNegotiation) {
+            gson {
+
+            }
+        }
+
         routing {
             get("/") {
-                call.respondHtml(HttpStatusCode.OK, HTML::index)
+                call.respond(HttpStatusCode.OK, "This is access-control: ${Instant.now()}")
+            }
+            get("/activate"){
+                call.respond(config)
             }
         }
     }.start(wait = true)
